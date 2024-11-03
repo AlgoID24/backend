50d76783be5d95720e20a003f0b84b7e2a5d146d
Ezekiel Victor <codeepoch@gmail.com>
2024-11-03 23:24:41 +0100

    add cors setup

--- algoid_backend/config/settings.py
+++ algoid_backend/config/settings.py
@@ -7,6 +7,7 @@ class Settings(BaseSettings):
     database_url: str = Field(default="")
     echo_sql: bool = Field(default=True)
     secret_key: str = Field(default="")
+    allowed_hosts: str = Field(default="")
     auth_token_token_valid_duration: timedelta = Field(default=timedelta(minutes=10))
     auth_token_refresh_token_valid_duration: timedelta = Field(
         default=timedelta(days=1)
--- algoid_backend/main.py
+++ algoid_backend/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from strawberry.fastapi import GraphQLRouter
+from fastapi.middleware.cors import CORSMiddleware
 
+from algoid_backend.config.settings import settings
 from algoid_backend.config.context import get_context
 from algoid_backend.config.schema import schema
 
@@ -8,4 +10,11 @@ from algoid_backend.config.schema import schema
 graphql_app = GraphQLRouter(schema=schema, context_getter=get_context)
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.allowed_hosts.split(","),
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(graphql_app, prefix="/graphql")
